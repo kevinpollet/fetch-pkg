3cbab8351e5e9e660bf6632c3f1bad0c0dbc6b20
kevinpollet <pollet.kevin@gmail.com>
2019-10-24 16:11:23 +0400

    refactor: throw an error if package is not found

--- src/fetchPkg.ts
+++ src/fetchPkg.ts
@@ -8,25 +8,24 @@
 import fetch, { json } from "npm-registry-fetch";
 import { Options } from "./Options";
 import { Package } from "./Package";
+import { PackageNotFoundError } from "./PackageNotFoundError";
 
 export const fetchPkg = async (
   name: string,
   opts?: Readonly<Options>
-): Promise<NodeJS.ReadableStream | undefined> => {
+): Promise<NodeJS.ReadableStream> => {
   let pkg;
   try {
     pkg = (await json(name, { ...opts })) as Package;
-  } catch(err) {
-    if (err.statusCode === 404) {
-      return undefined;
-    }
-    throw err;
+  } catch (err) {
+    throw err.statusCode === 404 ? new PackageNotFoundError(name) : err;
   }
 
   const versionToFetch = (opts && opts.version) || pkg["dist-tags"].latest;
   const version = pkg.versions[versionToFetch];
+  if (!version) {
+    throw new PackageNotFoundError(name);
+  }
 
-  return (
-    version && fetch(version.dist.tarball, { ...opts }).then(res => res.body)
-  );
+  return fetch(version.dist.tarball, { ...opts }).then(res => res.body);
 };
